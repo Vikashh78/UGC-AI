e81acc82204382597b9da3248ce6260c2a3b2e46
Vikashh78 <srmavikash10@gmail.com>
2026-04-13 13:15:29 +0530

    feat: integrate Cloudinary for image uploads and configure multer
    - Added Cloudinary dependency to package.json for image management.
    - Created cloudinary.js configuration file to set up Cloudinary with environment variables.
    - Added multer configuration in multer.js for handling file uploads.
    - Updated projectRoutes to include multer middleware for image uploads in the create project route.
    - Updated devDependencies in package.json to include @types/multer and updated multer version.

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -1,9 +1,71 @@
 import * as Sentry from '@sentry/node'
+import { prisma } from '../configs/prisma.js'
+import cloudinary from '../configs/cloudinary.js';
 
 // Create project
 const createProject = async (req, res) => {
+    
+    let tempProjectId;
+    const { userId } = req.auth()
+    let isCreditDeducted = false
+
+    const { name = 'New Project', aspectRatio, userPrompt, productName, productDescription, targetLength = 5} = req.body;
+
+    const images = req.files;
+
+    if(images.length < 2 || !productName) {
+        return res.status(400).json({
+            success: false,
+            message: 'Please upload at least two images'
+        })
+    }
+
+    const user = await prisma.user.findUnique({
+        where: { id: userId }
+    })
+
+    if(!user || user.credits < 5) {
+        return res.status(401).json({
+            success: false,
+            message: 'Insufficient credits'
+        })
+    } else {
+        // Deduct credits for image generation
+        await prisma.user.update({
+            where: { id: userId },
+            data: { credits: {decrement: 5} }
+        }).
+        then(() => {isCreditDeducted = true})
+    }
+     
     try {
-        
+        //"All images upload in parallel by using [promise.all()], and you get all URLs together after all uploads finish."
+        let uploadedImages = await Promise.all( 
+            images.map(async (item) => {
+                const result = await cloudinary.uploader.upload(
+                    item.path, 
+                    { resource_type: 'image'}
+                )
+                return result.secure_url
+            })
+        )
+
+        const project = await prisma.project.create({
+            data: {
+                name,
+                userId,
+                productName,
+                productDescription,
+                userPrompt,
+                aspectRatio,
+                targetLength: parseInt(targetLength),
+                uploadedImages,
+                isGenerating: true
+            }
+        })
+
+        tempProjectId = project.id;
+
     } catch (error) {
         Sentry.captureException(error)
         res.status(500).json({
